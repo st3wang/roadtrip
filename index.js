fea7e40db37b7db627aa0e3aa6cba4efdb72fa63
Steven Wang <st3wang@gmail.com>
2019-02-12 11:14:36 -0500

    fix url

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ async function getCandleDay(ymd,interval) {
 
 function downloadTradeDay(ymd) {
   return new Promise((resolve, reject) => {
-    const request = http.get(roadmap.url + '.csv.gz', function(response) {
+    const request = http.get(roadmap.url + ymd + '.csv.gz', function(response) {
       const csvFilename = 'data/trade/' + ymd + '.csv'
       const gzFilename = csvFilename + '.gz'
       const ws = fs.createWriteStream(gzFilename);
