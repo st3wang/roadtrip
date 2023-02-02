09c6abb446c262784712fe5dd21be0fecc017dc5
Steven Wang <st3wang@gmail.com>
2023-02-02 17:53:32 -0500

    a

--- journey/strategy/acc_strategy.js
+++ journey/strategy/acc_strategy.js
@@ -686,7 +686,7 @@ async function checkExit(tradeExchange) { try {
 
   var lastDistance = lastPrice - signal.signal.entryPrice
   console.log('lastDistance', lastDistance, 'lossDistance', signal.signal.lossDistance)
-  if (lastDistance >= -signal.signal.lossDistance) {
+  if (lastDistance >= 100) { //-signal.signal.lossDistance) {
     console.log('Move stop ', newStopLoss)
     let market = await tradeExchange.getCurrentMarket()
     let newStopLoss = getStopLoss(market,signal.signal.stopLossLookBack)
